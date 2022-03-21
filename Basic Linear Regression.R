0e5318218c1331883f7018516816a02b3bd0e541
Teofilo Acholla Ligawa Gafna <teofilo48ligawa@gmail.com>
2022-03-21 14:08:23 +0300

    Corrections on graphs

--- Basic Linear Regression.R
+++ Basic Linear Regression.R
@@ -100,17 +100,21 @@ income.graph
 
 ## We can add some style parameters using theme_bw() and making custom labels using labs().
 
-income.graph +
+graph_0<-income.graph +
     theme_bw() +
     labs(title = "Reported happiness as a function of income",
          x = "Income (x$10,000)",
          y = "Happiness score (0 to 10)")
+graph_0
+
 
 ### Multiple Linear Regression
 ## Use the function expand.grid() to create a dataframe with the parameters you supply.
 #| Within this function we will:
 #|Create a sequence from the lowest to the highest value of your observed biking data;
 #|Choose the minimum, mean, and maximum values of smoking, in order to make 3 levels of smoking over which to predict rates of heart disease.
+
+
 plotting.data<-expand.grid(
     biking = seq(min(heart.data$biking), max(heart.data$biking), length.out=30),
     smoking=c(min(heart.data$smoking), mean(heart.data$smoking), max(heart.data$smoking)))
@@ -124,24 +128,24 @@ plotting.data$predicted.y
 plotting.data$smoking <- round(plotting.data$smoking, digits = 2)
 
 ## switch the smoking to a factor
-plotting.data$smoking <- as.factor(plotting.data$smoking)
-plotting.data$smoking
+plotting.data_1<-as.factor(plotting.data$smoking)
+plotting.data_1
 
 ## Plot the original data
 heart.plot <- ggplot(heart.data, aes(x=biking, y=heart.disease)) +
     geom_point()
 
 ## To add regression line
-heart.plot <- heart.plot +
+heart.plot_0<- heart.plot +
     geom_line(data=plotting.data, aes(x=biking, y=predicted.y, color=smoking), size=1.25)
-heart.plot
+heart.plot_0
 
 ## Readying the graph
-heart.plot <-
-    heart.plot +
+heart.plot_1 <-
+    heart.plot_0 +
     theme_bw() +
     labs(title = "Rates of heart disease (% of population) \n as a function of biking to work and smoking",
          x = "Biking to work (% of population)",
          y = "Heart disease (% of population)",
          color = "Smoking \n (% of population)")
-heart.plot
+heart.plot_1
